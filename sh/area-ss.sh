936da164e00916fde91dd62fb656498f4f308bde
xXTgamerXx9844 <trinethr_n@protonmail.com>
2021-08-07 14:21:19 +0530

    Changes to be committed: 	modified:   area-ss.sh

--- sh/area-ss.sh
+++ sh/area-ss.sh
@@ -1,1 +1,1 @@
-scrot -l mode=edge,width=3,color="#5e81ac" -s 'area_%d-%m-%Y_%H:%M:%S'.png -e 'mv $f ~/Pictures/Screenshots' && play ~/sh/.screenshot.mp3
+scrot -l mode=edge,width=3,color="#5e81ac" -s $HOME/Pictures/Screenshots/'area_%d-%m-%Y_%H:%M:%S'.png && play ~/sh/.screenshot.mp3
